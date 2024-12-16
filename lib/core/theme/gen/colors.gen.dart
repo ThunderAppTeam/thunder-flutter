efe1d19ac3a9edc7eaca0caf70c8cb13f462e411
jrsky <jrsky723@gmail.com>
2024-12-16 12:28:04 +0900

    style: Color 추가

---
+++ lib/core/theme/gen/colors.gen.dart
@@ -0,0 +1,54 @@
+/// GENERATED CODE - DO NOT MODIFY BY HAND
+/// *****************************************************
+///  FlutterGen
+/// *****************************************************
+
+// coverage:ignore-file
+// ignore_for_file: type=lint
+// ignore_for_file: directives_ordering,unnecessary_import,implicit_dynamic_list_literal,deprecated_member_use
+
+import 'package:flutter/painting.dart';
+import 'package:flutter/material.dart';
+
+class ColorName {
+  ColorName._();
+
+  /// Color: #FE2C55
+  static const Color accentRed = Color(0xFFFE2C55);
+
+  /// Color: #000000
+  static const Color black = Color(0xFF000000);
+
+  /// Color: #E6E6E6
+  static const Color gray100 = Color(0xFFE6E6E6);
+
+  /// Color: #CCCCCC
+  static const Color gray200 = Color(0xFFCCCCCC);
+
+  /// Color: #B3B3B3
+  static const Color gray300 = Color(0xFFB3B3B3);
+
+  /// Color: #999999
+  static const Color gray400 = Color(0xFF999999);
+
+  /// Color: #F2F2F2
+  static const Color gray50 = Color(0xFFF2F2F2);
+
+  /// Color: #808080
+  static const Color gray500 = Color(0xFF808080);
+
+  /// Color: #666666
+  static const Color gray600 = Color(0xFF666666);
+
+  /// Color: #4D4D4D
+  static const Color gray700 = Color(0xFF4D4D4D);
+
+  /// Color: #333333
+  static const Color gray800 = Color(0xFF333333);
+
+  /// Color: #1A1A1A
+  static const Color gray900 = Color(0xFF1A1A1A);
+
+  /// Color: #FFFFFF
+  static const Color white = Color(0xFFFFFFFF);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData.dark(
         useMaterial3: true,
       ).copyWith(
+        scaffoldBackgroundColor: Colors.black,
         extensions: [
           defaultTextTheme,
         ],
