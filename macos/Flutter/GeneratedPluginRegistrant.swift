e33cd17e4e23c12bc2055bf8a99d781f718fce70
jrsky <jrsky723@gmail.com>
2025-01-27 22:30:03 +0900

    feat: Add share functionality to body check result
    - Add share_plus package for sharing functionality
    - Implement screenshot capture using RepaintBoundary
    - Add loading state for share button

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -12,6 +12,7 @@ import flutter_image_compress_macos
 import flutter_secure_storage_macos
 import path_provider_foundation
 import photo_manager
+import share_plus
 import shared_preferences_foundation
 import webview_flutter_wkwebview
 
@@ -23,6 +24,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterSecureStoragePlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStoragePlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
+  SharePlusMacosPlugin.register(with: registry.registrar(forPlugin: "SharePlusMacosPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
   FLTWebViewFlutterPlugin.register(with: registry.registrar(forPlugin: "FLTWebViewFlutterPlugin"))
 }
