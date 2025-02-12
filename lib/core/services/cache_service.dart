d970a07e13d4789285858e7b2344d2e97f1b7101
jrsky <jrsky723@gmail.com>
2025-02-12 17:45:45 +0900

    feat: improve cache service to logging

--- lib/core/services/cache_service.dart
+++ lib/core/services/cache_service.dart
@@ -7,6 +7,7 @@ import 'package:thunder/core/services/log_service.dart';
 class CacheService {
   static const int _maxCacheAgeDays = 7;
   static const int _maxCacheSizeMB = 50;
+  static const int _mbSize = 1024 * 1024;
 
   static Future<Directory> _getImagesCacheDirectory() async {
     final cacheDir = await getTemporaryDirectory();
@@ -23,63 +24,105 @@ class CacheService {
   }
 
   static Future<void> cleanCache() async {
-    LogService.info('Cleaning cache');
-    await cleanOldCache();
-    await limitCacheSize();
-  }
-
-  // 오래된 캐시만 정리
-  static Future<void> cleanOldCache() async {
-    try {
-      final cacheDir = await getTemporaryDirectory();
-      final now = DateTime.now();
-      if (await cacheDir.exists()) {
-        await for (var entity in cacheDir.list(recursive: true)) {
-          if (entity is File) {
-            final lastModified = entity.statSync().modified;
-            if (now.difference(lastModified).inDays >= _maxCacheAgeDays) {
-              await entity.delete();
-            }
-          }
-        }
+    LogService.info('Starting cache cleanup...');
+    final cacheDir = await getTemporaryDirectory();
+    if (await cacheDir.exists()) {
+      final oldCacheResult = await cleanOldCache(cacheDir);
+      final sizeLimitResult = await limitCacheSize(cacheDir);
+      // 최종 결과 로깅
+      if (oldCacheResult.deletedCount > 0 || sizeLimitResult.deletedCount > 0) {
+        LogService.debug('Cache cleanup completed:\n'
+            '- Old files removed: ${oldCacheResult.deletedCount} (${oldCacheResult.deletedSizeMB.toStringAsFixed(2)}MB)\n'
+            '- Size limit cleanup: ${sizeLimitResult.deletedCount} (${sizeLimitResult.deletedSizeMB.toStringAsFixed(2)}MB)');
+      } else {
+        LogService.debug('No cache cleanup needed');
       }
-    } catch (e) {
-      LogService.error('Cache cleanup error: $e');
     }
   }
 
-  // 캐시 크기 제한
-  static Future<void> limitCacheSize() async {
+// 오래된 캐시만 정리
+  static Future<CleanupResult> cleanOldCache(Directory cacheDir) async {
     try {
-      final cacheDir = await getTemporaryDirectory();
-      int totalSize = 0;
-
-      final files = await cacheDir
-          .list(recursive: true)
-          .where((entity) => entity is File)
-          .toList();
-
-      for (var file in files) {
-        if (file is File) {
-          totalSize += await file.length();
-        }
-      }
-
-      // 최대 크기 초과시 오래된 파일부터 삭제
-      if (totalSize > _maxCacheSizeMB * 1024 * 1024) {
-        files.sort(
-            (a, b) => a.statSync().modified.compareTo(b.statSync().modified));
-
-        for (var file in files) {
-          if (file is File) {
-            await file.delete();
-            totalSize -= await file.length();
-            if (totalSize <= _maxCacheSizeMB * 1024 * 1024) break;
+      final now = DateTime.now();
+      final targetFiles = <FileInfo>[];
+      int deletedCount = 0;
+      int deletedSize = 0;
+      await for (var entity in cacheDir.list(recursive: true)) {
+        if (entity is File) {
+          try {
+            final stat = entity.statSync();
+            if (now.difference(stat.modified).inDays >= _maxCacheAgeDays) {
+              targetFiles.add(FileInfo(entity, stat.size, stat.modified));
+            }
+          } catch (_) {
+            continue;
           }
         }
       }
+      for (var fileInfo in targetFiles) {
+        await fileInfo.file.delete();
+        deletedSize += fileInfo.size;
+        deletedCount++;
+      }
+      return CleanupResult(deletedCount, deletedSize / _mbSize);
+    } catch (e) {
+      LogService.error('Old cache cleanup error: $e');
+      return CleanupResult(0, 0);
+    }
+  }
+
+  static Future<CleanupResult> limitCacheSize(Directory cacheDir) async {
+    try {
+      int totalSize = 0;
+      final validFiles = <FileInfo>[];
+      await for (final entity in cacheDir.list(recursive: true)) {
+        if (entity is File) {
+          try {
+            final stat = entity.statSync();
+            totalSize += stat.size;
+            validFiles.add(FileInfo(entity, stat.size, stat.modified));
+          } catch (_) {
+            continue;
+          }
+        }
+      }
+      if (totalSize > _maxCacheSizeMB * _mbSize) {
+        validFiles.sort((a, b) => a.modified.compareTo(b.modified));
+        int deletedCount = 0;
+        int deletedSize = 0;
+
+        for (var fileInfo in validFiles) {
+          try {
+            await fileInfo.file.delete();
+            deletedSize += fileInfo.size;
+            deletedCount++;
+            if ((totalSize - deletedSize) <= _maxCacheSizeMB * _mbSize) {
+              break;
+            }
+          } catch (_) {
+            continue;
+          }
+        }
+        return CleanupResult(deletedCount, deletedSize / _mbSize);
+      }
+      return CleanupResult(0, 0);
     } catch (e) {
       LogService.error('Cache size limit error: $e');
+      return CleanupResult(0, 0);
     }
   }
+}
+
+// 파일 정보를 담을 클래스
+class FileInfo {
+  final File file;
+  final int size;
+  final DateTime modified;
+  FileInfo(this.file, this.size, this.modified);
+}
+
+class CleanupResult {
+  final int deletedCount;
+  final double deletedSizeMB;
+  CleanupResult(this.deletedCount, this.deletedSizeMB);
 }
